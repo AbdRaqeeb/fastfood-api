bf16d0f622fa41fd65b62927280a46e90cfa7a03
Horlarmeday <ajaomahmud@gmail.com>
2020-09-18 18:23:15 +0100

    Chore(Updated food controller)

--- server/modules/food/controllers/FoodController.js
+++ server/modules/food/controllers/FoodController.js
@@ -29,7 +29,7 @@ class FoodController {
                 where:
                     {
                         name,
-                        category
+                        category_id
                     }
             });
 
